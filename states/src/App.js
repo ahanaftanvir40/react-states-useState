b94edc70f2f1a9aba05c0c8cd705440aad23c86f
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-01-11 05:11:31 +0600

    up

--- states/src/App.js
+++ states/src/App.js
@@ -16,6 +16,21 @@ function App() {
     setCount(0)
   }
 
+
+
+  const [tasks, setTasks] = useState([
+    { id: 1, name: 'Do your homework', completed: true },
+    { id: 2, name: 'Web dev work', completed: false },
+    { id: 3, name: 'Meeting with faculty', completed: false }
+  ])
+
+
+
+  let taskDel = (taskId) => {
+    setTasks(tasks.filter((task) => taskId !== task.id))
+  }
+
+
   return (
     <div className="App">
       <div className="box">
@@ -24,6 +39,18 @@ function App() {
       <button onClick={addnum} className='add' >Add</button>
       <button onClick={subnum} className='sub'>Sub</button>
       <button onClick={reset} className='sub'>Reset</button>
+
+      <h1>Task List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span>{task.id} - {task.name}</span>
+            <button onClick={() => taskDel(task.id)} className='delete'>Delete</button>
+          </li>
+        ))}
+
+      </ul>
+
     </div>
   );
 }
