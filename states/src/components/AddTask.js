18da3559b4f10f31ee853cd7ca2b1da21c915a18
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-01-16 11:54:55 +0600

    up

--- states/src/components/AddTask.js
+++ states/src/components/AddTask.js
@@ -1,14 +1,22 @@
 import './AddTask.css'
-
+import { useState } from 'react'
 export const AddTask = () => {
+
+    const [taskValue, setTaskValue] = useState('')
+
+    const handleReset = () => {
+        setTaskValue('')
+    }
     return (
         <div>
             <section className="addtask">
                 <form action="">
                     <label htmlFor="task">Add Task</label>
-                    <input type="text" name="task" id="task" placeholder="Task Name" />
+                    <input onChange={(e) => setTaskValue(e.target.value)} type="text" value={taskValue} name="task" id="task" placeholder="Task Name" autoComplete='off' />
+                    <span onClick={handleReset} className='reset'>Reset</span>
                     <button type="submit">Add</button>
                 </form>
+                <p>{taskValue}</p>
             </section>
         </div>
     )
